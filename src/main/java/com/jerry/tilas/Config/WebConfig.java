feeadb666042c85209d748176e6977e206ebaaf0
Jerry <1369161009@qq.com>
2026-08-13 00:39:25 +0800

    8.13 commit

--- src/main/java/com/jerry/tilas/Config/WebConfig.java
+++ src/main/java/com/jerry/tilas/Config/WebConfig.java
@@ -13,6 +13,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(demoInterCeptor).addPathPatterns("/**");
+        registry.addInterceptor(demoInterCeptor).addPathPatterns("/**").excludePathPatterns("/login");
     }
 }
--- src/main/java/com/jerry/tilas/aop/RecordTimeAspect.java
+++ src/main/java/com/jerry/tilas/aop/RecordTimeAspect.java
@@ -2,16 +2,23 @@ package com.jerry.tilas.aop;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.*;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
-@Aspect
+//动态代理（依赖注入）
+@Aspect//切面类
+@Order(1)//等级
 @Slf4j
 @Component
 public class RecordTimeAspect {
+    //连接点——切入点——目标对象——通知——切面
+    @Pointcut("execution(* com.jerry.tilas.service.*.*(..))")//切入点表达式
+    public void pt() {}
 
+    //环绕（需指定运行位置 ProcessdingJoinPoint->process）
     @Around("execution(* com.jerry.tilas.service.*.*(..))")
     public Object recordTime(ProceedingJoinPoint joinPoint) throws Throwable {
             long beginTime = System.currentTimeMillis();
@@ -20,7 +27,25 @@ public class RecordTimeAspect {
             log.info("{}用时{}ms",joinPoint.getSignature().getName(),(endTime - beginTime));
             return result;
     }
-
-
+    //之前运行
+    @Before("pt()")
+    public void before(JoinPoint joinPoint) throws Throwable {
+        log.info("before");
+    }
+    //之后运行
+    @After("execution(* com.jerry.tilas.service.*.*(..))")
+    public void after(JoinPoint joinPoint) throws Throwable {
+        log.info("after");
+    }
+    //异常后不会运行
+    @AfterReturning("execution(* com.jerry.tilas.service.*.*(..))")
+    public void afterReturning(JoinPoint joinPoint) throws Throwable {
+        log.info("afterReturning");
+    }
+    //异常后才运行
+    @AfterThrowing("execution(* com.jerry.tilas.service.*.*(..))")
+    public void afterThrowing(JoinPoint joinPoint){
+        log.info("afterThrowing");
+    }
 
 }
